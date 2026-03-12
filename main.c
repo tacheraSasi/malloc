293f5e4cb96d58573708f7212e4d486e863a27eb
tacheraSasi <tacherasasi@gmail.com>
2026-03-12 16:16:36 +0300

    refactor main function to allocate memory using my_malloc

--- main.c
+++ main.c
@@ -13,8 +13,12 @@ void *my_malloc(size_t size){
 
 int main()
 {
-    
-    printf("Hello, World!");
+    void *ptr = my_malloc(100);
+    if(ptr == NULL){
+        return 1;
+    }
+
+    printf("Memory allocated at: %p\n", ptr);
     return 0;
 }
 
