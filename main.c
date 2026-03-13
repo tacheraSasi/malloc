7edc760b843cb45f12f7a29269ad86cf6c2d2a7d
tacheraSasi <tacherasasi@gmail.com>
2026-03-13 12:03:12 +0300

    add main function to demonstrate memory allocation and deallocation using my_malloc and my_calloc

--- main.c
+++ main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include "my_malloc.h"
 
 int main()
 {
-    void *ptr = my_malloc(100);
-    if (ptr == NULL)
-    {
-        return 1;
-    }
+    
+    int *arr = (int *)my_malloc(5 * sizeof(int));
+    for (int i = 0; i < 5; i++)
+        arr[i] = i * 10;
+    for (int i = 0; i < 5; i++)
+        printf("%d ", arr[i]); // 0 10 20 30 40
+    printf("\n");
 
-    printf("Memory allocated at: %p\n", ptr);
+    my_free(arr);
+
+    
+    int *zeros = (int *)my_calloc(5, sizeof(int));
+    for (int i = 0; i < 5; i++)
+        printf("%d ", zeros[i]); // 0 0 0 0 0
+    printf("\n");
+
+    // my_free(zeros);
+
     return 0;
 }
