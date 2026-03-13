3d7f149ac36b8bfb8fd843f0766deb14d18a1385
tacheraSasi <tacherasasi@gmail.com>
2026-03-13 11:36:38 +0300

    add block_header struct for memory management

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<unistd.h>
 
+typedef struct block_header {
+    size_t size;
+    int free;
+    struct block_header *next;
+} block_header_t;
+
 void *my_malloc(size_t size){
     void* ptr = sbrk(size);
     if(ptr == (void*) -1){
